8bbd5c872b3820a158830687e5d7faddd9f08a0d
Aguirre <uidw7375@contiwan.com>
2017-11-18 12:39:36 -0600

    [FFT] Update with files - Still some issues with the linker

--- source/appl/main.c
+++ source/appl/main.c
@@ -5,8 +5,12 @@
 #include "board.h"
 #include "app_scheduler.h"
 #include "Tasks.h"    
+#include "fpu.h"   
 #include <stdbool.h>
 #include <stdio.h>
+/**  Fast fourier transform */
+#include    "fft.h"
+#include    "ecg_data.h"
 
 /*----------------------------------------------------------------------------
  *        Local definitions
@@ -14,6 +18,20 @@
 /*@Yisus Adc struct*/
 #define BOARD_MCK    150000000 
 #define RxBufferDMASize   8
+
+/*~~~~~~  Local definitions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+#define TEST_LENGTH_SAMPLES 2048
+/*~~~~~~  Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/** Auxiliary input buffer to accomodate data as FFT function expects it */
+float       fft_inputData[TEST_LENGTH_SAMPLES];
+/** Output magnitude data */
+float       fft_signalPower[TEST_LENGTH_SAMPLES/2];
+/** Auxiliary output variable that holds the frequency bin with the highest level of signal power */
+uint32_t    u32fft_maxPowerIndex;
+/** Auxiliary output variable that holds the maximum level of signal power */
+float       fft_maxPower;
+
 uint32_t *RxDMABuffer;
 extern Afec pAfe_ADC0;
 extern Pwm pPwm_0;                
@@ -60,7 +78,8 @@ static void _ConfigureLeds( void )
  */
 extern int main( void )
 {
-
+	/** Auxiliary array index */
+    uint16_t    u16index;
 	/* Disable watchdog */
 	WDT_Disable( WDT ) ;
 
@@ -72,7 +91,8 @@ extern int main( void )
 	/* Enable I and D cache */
 	//SCB_EnableICache();
   //SCB_EnableDCache();
-
+    /* Enable Floating Point Unit */
+    vfnFpu_enable();
 	printf( "Configure LED PIOs.\n\r" ) ;
 	_ConfigureLeds() ;
   
@@ -158,8 +178,20 @@ XDMAD_IsTransferDone();
 	
   /*-- Loop through all the periodic tasks from Task Scheduler --*/
 	for(;;)
-	{
-		/* Perform all scheduled tasks */
+    {	
+		/*Prepare data for FFT operation */
+        for (u16index = 0; u16index < (TEST_LENGTH_SAMPLES/2); u16index++)
+        {
+            fft_inputData[(2*u16index)] = ecg_resampled[u16index];
+            fft_inputData[(2*u16index) + 1] = 0;
+        }
+        /** Perform FFT on the input signal */
+        fft(fft_inputData, fft_signalPower, TEST_LENGTH_SAMPLES/2, &u32fft_maxPowerIndex, &fft_maxPower);
+        
+        /* Publish through emulated Serial the byte that was previously sent through the regular Serial channel */
+		printf("%5d  %5.4f \r\n", u32fft_maxPowerIndex, fft_maxPower);
+		
+        /** Perform periodically activated tasks */
 		vfnTask_Scheduler();
        
 
